abe080e51be6550bd2f2dd1c80a6c7e1fad33f53
Mahmoud Nefzi <mahmoudhnefzi@gmail.com>
2024-05-29 22:07:15 +0100

    ui: implemented homescreen ui

---
+++ src/components/general/navbar.tsx
@@ -0,0 +1,27 @@
+import Link from "next/link";
+import Image from "next/image";
+
+const Navbar = () => {
+  return (
+    <nav className="text-black p-4 flex justify-between items-center">
+      <div className="logo font-bold text-lg">
+        <Link href="/" className="flex items-center space-x-3 text-[#00BBF9]">
+          <Image
+            src={"/clipboard.png"}
+            width={50}
+            height={50}
+            alt={"Logo"}
+          ></Image>
+          <span>Clipboard</span>
+        </Link>
+      </div>
+      <div className="space-x-3">
+        <Link href="/about">List</Link>
+        <Link href="/about">About</Link>
+        <Link href="/about" className="rounded-xl bg-yellow-300 p-2 px-3">+ Create</Link>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
---
+++ src/components/notes/notes-list.tsx
@@ -0,0 +1,44 @@
+import Link from "next/link";
+import Image from "next/image";
+
+const notes = [
+  {
+    id: 1,
+    title: "My first note",
+    content: "This is my first note. I'm so excited to use Clipboard!",
+    createdAt: new Date(),
+  },
+  {
+    id: 2,
+    title: "My second note",
+    content: "This is my second note. I'm so excited to use Clipboard!",
+    createdAt: new Date(),
+  },
+  {
+    id: 3,
+    title: "My third note",
+    content: "This is my third note. I'm so excited to use Clipboard!",
+    createdAt: new Date(),
+  },
+];
+
+const NotesList = () => {
+  return (
+    <div className="flex flex-col">
+      {notes.map((note) => (
+        <Link href={`/notes/${note.id}`} key={note.id}>
+          <div className="p-4 bg-gray-100 rounded-lg my-2 cursor-pointer">
+            <h2 className="text-lg font-bold">{note.title}</h2>
+            <p className="text-gray-700">{note.content}</p>
+            <p className="text-gray-500 text-sm">
+              {note.createdAt.toLocaleString()}
+            </p>
+            <h4 className="text-[#F15BB5] font-semibold">Read More {"->"}</h4>
+          </div>
+        </Link>
+      ))}
+    </div>
+  );
+};
+
+export default NotesList;
